cc3966ca851bd7d23591fe0affb56d5bb356ad34
rafaelrendonx <rafael_x@hotmail.com>
2022-12-30 10:28:20 -0700

    Actualizacion

--- index.js
+++ index.js
@@ -7,42 +7,42 @@ let compras = [];
 
 
 enviarFormulario.addEventListener("submit", function (event) {
-    event.preventDefault();                          
+    event.preventDefault();
     if (enviarProducto.value.trim() !== '' && enviarCantidad.value.trim() !== '' && enviarProducto.value.trim() !== '') {
         crearTabla.innerHTML = '';
         agregarProducto(enviarProducto.value, enviarCantidad.value, enviarEstablecimiento.value);
         mostrarProducto();
         event.target.reset();
-    } else{
+    } else {
         alert("Los 3 campos son necesarios.")
     }
 });
 
-function agregarProducto (producto, cantidad, establecimiento) {
+function agregarProducto(producto, cantidad, establecimiento) {
     compras.push({
         producto,
-        cantidad : cantidad,
-        establecimiento : establecimiento,
+        cantidad: cantidad,
+        establecimiento: establecimiento,
     })
 }
 
-function mostrarProducto () {
+function mostrarProducto() {
     crearTabla.innerHTML = '';
     compras.forEach(function (objeto, indice) {
         crearTabla.innerHTML += `<tr>
-        <th>${indice+1}</th>       
-        <td>${objeto.producto}</td>
-        <td>${objeto.cantidad}</td>
-        <td>${objeto.establecimiento}</td>
+        <th class="align-middle">${indice + 1}</th>       
+        <td class="align-middle">${objeto.producto}</td>
+        <td class="align-middle">${objeto.cantidad}</td>
+        <td class="align-middle">${objeto.establecimiento}</td>
         <td><button onclick="editarProducto(${indice})" class="btn btn-warning">Editar</button></td>
         <td><button onclick="eliminarProducto(${indice})" class="btn btn-danger">Eliminar</button></td>
         </tr>`
     })
 
-    guardarProductosStorage ();
+    guardarProductosStorage();
 }
 
-function editarProducto (indice) {
+function editarProducto(indice) {
     compras[indice].producto = prompt("Ingresa un nuevo producto:");
     compras[indice].cantidad = prompt("Ingresa una nueva cantidad:");
     compras[indice].establecimiento = prompt("Ingresa un nuevo establecimiento:");
@@ -50,20 +50,20 @@ function editarProducto (indice) {
     mostrarProducto();
 }
 
-function eliminarProducto (indice) {
+function eliminarProducto(indice) {
     compras.splice(indice, 1);
 
     mostrarProducto();
 }
 
-function guardarProductosStorage () {
+function guardarProductosStorage() {
     const productosGuardar = JSON.stringify(compras);
     console.log(compras)
     localStorage.setItem("compras", productosGuardar);
 }
- 
-function cargarProductosStorage () {
-    const productosStorage = localStorage.getItem("compras"); 
+
+function cargarProductosStorage() {
+    const productosStorage = localStorage.getItem("compras");
     if (productosStorage == null) {
         compras = [];
     } else {
